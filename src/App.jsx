f79dfd813f21bec875d301f00a8216b2ddcd55a1
AnPham <anpham1122000@gmail.com>
2023-10-22 19:56:36 +0700

    add lint fix and color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
 import Button from '@mui/material/Button'
+import {
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <ModeToggle />
       <p>Material UI</p>
       <Button variant='text'>text</Button>
       <Button variant='contained'>contained</Button>
