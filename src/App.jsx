e6637362ceb2ad16b935a0927127da5fc38b4f9e
AnPham <anpham1122000@gmail.com>
2023-10-27 23:02:51 +0700

    refactor project structure and add absolute path

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-import ModeSelect from './components/ModeSelect'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 
-function App() {
+import AppBar from '~/components/AppBar'
+import Board from './pages/Boards/_id'
 
+function App() {
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-      <Box sx={{
-        backgroundColor: 'primary.light',
-        width: '100%',
-        height: (theme) => theme.trelloCustom.appBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        <ModeSelect />
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: 'primary.dark',
-          width: '100%',
-          height: (theme) => theme.trelloCustom.boardBarHeight,
-          display: 'flex',
-          alignItems: 'center'
-        }}
-      >
-      Board bar
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.main',
-        width: '100%',
-        height: (theme) => `calc(100vh - ${theme.trelloCustom.appBarHeight} - ${theme.trelloCustom.boardBarHeight})`,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-      Board content
-      </Box>
-    </Container>
+    <>
+      <Board />
+    </>
   )
 }
 
