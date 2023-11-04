ab48e2daf6f526d1be0523b812f2b65d0a1f8827
AnPham <anpham1122000@gmail.com>
2023-11-04 23:32:05 +0700

    update-app-bar-part-2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -29,7 +29,7 @@ function AppBar() {
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }}/>
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-          <SvgIcon component={trelloLogo} inheritViewBox />
+          <SvgIcon component={trelloLogo} inheritViewBox sx={{ color: 'primary.main' }}/>
           <Typography
             variant='span'
             sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}
@@ -49,11 +49,11 @@ function AppBar() {
         <ModeSelect />
         <Tooltip title="Notification">
           <Badge color='secondary' variant='dot' sx={{ cursor: 'pointer' }}>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }} />
           </Badge>
         </Tooltip>
         <Tooltip title="Notification">
-          <HelpOutlineIcon />
+          <HelpOutlineIcon sx={{ color: 'primary.main' }} />
         </Tooltip>
 
         <Profiles />
